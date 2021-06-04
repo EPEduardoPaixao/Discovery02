78de25a4d7735eca9a04e2cfb0ecd4eb980478e8
Eduardo Cavalcante <42215395+EPEduardoPaixao@users.noreply.github.com>
2021-06-04 01:06:10 -0300

    consonantização do header

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,19 @@ const routes = express.Router();
 //O EJS LLER POR PADRÃO A PASSTA VIEWS
 const views = __dirname+'/views/'
 
+
+const profile = {
+    name: 'Paixão',
+    avatar:'https://avatars.githubusercontent.com/u/42215395?v=4',
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    'hours-per-day': 5,
+    'vacation-per-year': 4,
+}
+
 routes.get("/", (req, res) => res.render(views+"index"));
 routes.get("/job", (req, res) => res.render(views+"job"));
 routes.get("/job/edit", (req, res) => res.render(views+"job-edit"));
-routes.get("/profile", (req, res) => res.render(views+"profile"));
+routes.get("/profile", (req, res) => res.render(views+"profile", {profile}));
 
 module.exports = routes; 
